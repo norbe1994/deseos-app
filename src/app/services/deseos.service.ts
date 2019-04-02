c791cc859fd593bfca159bc6ae4c8e6d71798475
Norberto Caceres <norbe1994@yahoo.com>
2019-04-02 11:28:45 -0500

    implementar localStorage para las listas

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -8,13 +8,24 @@ export class DeseosService {
 	listas: Lista[] = []
 
 	constructor() {
-		const lista1 = new Lista('tareas1')
-		const lista2 = new Lista('tareas2')
-		this.listas.push(lista1, lista2)
+		this.cargarStorage()
 	}
 
 	crearLista(titulo: string) {
 		const nuevaLista = new Lista(titulo)
 		this.listas.push(nuevaLista)
+		this.guardarStorage()
+	}
+
+	guardarStorage() {
+		localStorage.setItem('data', JSON.stringify(this.listas))
+	}
+
+	cargarStorage() {
+		if (localStorage.getItem('data')) {
+			this.listas = JSON.parse(localStorage.getItem('data'))
+		} else {
+			this.listas = []
+		}
 	}
 }
