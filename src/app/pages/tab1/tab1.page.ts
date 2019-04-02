26cdd1fc5b016592a6e43a317cfc9676828c1a97
Norberto Caceres <norbe1994@yahoo.com>
2019-04-02 10:25:45 -0500

    crear pagina de agregar lista y ruteo basico entre tab1 y agregar

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core'
 import { DeseosService } from 'src/app/services/deseos.service'
+import { Router } from '@angular/router'
 
 @Component({
 	selector: 'app-tab1',
@@ -7,5 +8,9 @@ import { DeseosService } from 'src/app/services/deseos.service'
 	styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-	constructor(public deseosService: DeseosService) {}
+	constructor(public deseosService: DeseosService, private router: Router) {}
+
+	agregarLista() {
+		this.router.navigateByUrl('/tabs/tab1/agregar')
+	}
 }
